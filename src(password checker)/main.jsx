7986877cd8d482de1e3acbee2a54ab35d4749355
DhanavathAkhil <151422570+DhanavathAkhil@users.noreply.github.com>
2024-09-12 17:49:26 +0530

    Update main.jsx

--- src(password checker)/main.jsx
+++ src(password checker)/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from "../App";  // Correct path for App component
+import App from "./App";  // Correct path for App component
  // Adjust the path based on where App.jsx is located
 import './index.css'; // Optional: For additional styling
 
